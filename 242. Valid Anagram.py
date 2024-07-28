1b9bcfa95d95ac19ccd2cbf8ede9d3b6933a09b1
Md Abdur Rahman Emad <17620076+iemad@users.noreply.github.com>
2024-07-28 19:15:57 +0300

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -19,4 +19,23 @@ class Solution:
                 dict_t[t[i]] = 1
 
         return dict_s == dict_t
+
+'''Easier Way Below'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        dict_s, dict_t = [], []
+
+        for i in s:
+            dict_s.append(i)
+        dict_s = sorted(dict_s) 
+        
+        for i in t:
+            dict_t.append(i)
+        dict_t = sorted(dict_t) 
+
+        return dict_s == dict_t
         
