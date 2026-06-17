b20d4615d0525c71bce5aac84f6146c97207bece
Md Abdur Rahman Emad <17620076+iemad@users.noreply.github.com>
2026-06-17 23:36:38 +0300

    Update Find Closest Number to Zero.py

--- ALGOMAP/Arrays & Strings/Find Closest Number to Zero.py
+++ ALGOMAP/Arrays & Strings/Find Closest Number to Zero.py
@@ -1,3 +1,4 @@
+## Solution 1:
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
         closest = nums[0]
@@ -8,3 +9,8 @@ class Solution:
                 if num > closest:
                     closest = num
         return closest
+
+Solution 2:
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        return min(nums, key=lambda x: (abs(x), -x))
